ce1d6737bdfc3a250ccb6f9c846a953f0ce2f000
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-02-06 08:02:15 -0800

    add some

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 let bars = document.querySelector(".fa-bars");
-let xmark = document.querySelector(".fa-xmark");
-console.log(xmark);
-let menu = document.querySelector('.menu')
-
-bars.addEventListener('click', function () {
-    menu.style.transform = 'translateX(0)';
-    menu.style.transition = 'all 0.5s ease-in-out';
+let x = document.querySelector(".menu button");
+let menu = document.querySelector(".menu");
+let sun = document.querySelector(".fa-sun");
+let moon = document.querySelector(".fa-moon");
+bars.addEventListener('click', function() {
+   menu.style.cssText = "transform: translateX(0); transition: all 0.5s ease;);";
+})
+x.addEventListener('click', function()  {
+    menu.style.cssText = "transform: translateX(-300px); transition: all 0.5s ease;";
 })
 
-xmark.addEventListener('click', function () {
-    menu.style.transform = 'translateX(-300px)';
-    menu.style.transition = 'all 0.5s ease-in-out';
+moon.addEventListener('click', function()  {
+    document.body.style.cssText = "background-color: rgb(15, 14, 14); color: white; transition: all 0.5s ease;"
+    moon.style.cssText = " transform: scale(1.5); transition: all 0.5s ease;";
+    sun.style.cssText = "color: yellow; transition: all 0.5s ease;";
+})
+sun.addEventListener('click', function()  {
+    document.body.style.cssText = "background-color: white; transition: all 0.5s ease;";
+    sun.style.cssText = "transform: scale(1.5); transition: all 0.5s ease;";
 })
 
