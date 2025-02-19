ad16aa09a833c868d7d81d538d3da4a6c0ec054f
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-02-19 08:03:59 -0800

    update diff codesss

--- main.js
+++ main.js
@@ -8,20 +8,33 @@
 //    yangiDiv.style.top = e.clientY + 'px';
 // });
 
-let btn = document.querySelector('.btn');
+// let btn = document.querySelector('.btn');
 
 let hexColur = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-btn.addEventListener('click', () => {
+// btn.addEventListener('click', () => {
+//     let color = '#';
+//     for (let i = 0; i < 6; i++) {
+//         let random = Math.floor(Math.random() * hexColur.length);
+//         color += hexColur[random];
+//         console.log(color);
+//     }
+//     document.body.style.backgroundColor = color;
+// });
+
+
+document.body.addEventListener('mousemove', (e) => {
     let color = '#';
     for (let i = 0; i < 6; i++) {
         let random = Math.floor(Math.random() * hexColur.length);
         color += hexColur[random];
-        console.log(color);
     }
-    document.body.style.backgroundColor = color;
-});
-
-
+    let yangiDiv = document.createElement('div');
+    document.body.appendChild(yangiDiv);
+    yangiDiv.classList.add('container');
+    yangiDiv.style.left = e.clientX + 'px';
+    yangiDiv.style.top = e.clientY + 'px';
+    yangiDiv.style.backgroundColor = color;
+})
 
 
