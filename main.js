74d6e23a8c925ca33b636ac1b73b43513c94dca0
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-02-19 07:40:44 -0800

    update diff code

--- main.js
+++ main.js
@@ -1,36 +1,12 @@
-// filter
-// arrow funksiya -- =>
-//function()
-// ()=>
-
-// let sonlar = [1,2,3,4,5,6,7,8,9,10];
-
-// let filterlash = sonlar.filter(tesha => tesha % 3 == 0);
-// console.log(filterlash);
-
-// let filterOndanKatta = sonlar.filter(bolta => bolta > 5);
-// console.log(filterOndanKatta);
-
-
-// let ismlar = ["Ilyosbek", "Marjona", "Muhamadali", "Abdulatif", "Bekzodbek", "Azamat", "Po'lat"];
-
-// // let filterlash = ismlar.filter(isim => isim.length > 8);
-// // console.log(filterlash);
-
-// let BoshlFilter =ismlar.filter(pepsi => pepsi.charAt(0) == "P");
-// console.log(BoshlFilter);
-
-// //male - erkak
-// //female - ayol
-// let users = [
-//     { name: "Shukurulloh", age: 18, gender: "male", yili: '2007-11-22'},
-//     {name: "Azamat", age: 29, gender: "male", yili: '2002-11-22'},
-//     {name: "Sug'diyone", age:21, gender: "female", yili: '2004-11-22'},
-// ]
-
-
-// let FilterYili = users.filter(yil => new Date(yil.yili) > new Date('2007-01-01'));
-// console.log(FilterYili);
+const btn = document.querySelector('.btn');
+const container = document.querySelector('.container');
+document.addEventListener('mousemove', (e) => {
+   let yangiDiv = document.createElement('div');
+   document.body.appendChild(yangiDiv);
+   yangiDiv.classList.add('container');
+   yangiDiv.style.left = e.clientX + 'px';
+   yangiDiv.style.top = e.clientY + 'px';
+});
 
 
 
