dd6c7ab617df8f4e66a00a571b4bd165603ffbc7
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-02-19 08:09:22 -0800

    update diff codedsjabawkadk.sa,jdakdsj

--- main.js
+++ main.js
@@ -23,18 +23,22 @@ let hexColur = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 // });
 
 
-document.body.addEventListener('mousemove', (e) => {
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        let random = Math.floor(Math.random() * hexColur.length);
-        color += hexColur[random];
+document.body.addEventListener('keydown', (e) => {
+    if (e.key === 'h') {
+        
+            let color = '#';
+            for (let i = 0; i < 6; i++) {
+                let random = Math.floor(Math.random() * hexColur.length);
+                color += hexColur[random];
+            }
+            let yangiDiv = document.createElement('div');
+            document.body.appendChild(yangiDiv);
+            yangiDiv.classList.add('container');
+            yangiDiv.style.left = e.clientX + 'px';
+            yangiDiv.style.top = e.clientY + 'px';
+            yangiDiv.style.backgroundColor = color;
     }
-    let yangiDiv = document.createElement('div');
-    document.body.appendChild(yangiDiv);
-    yangiDiv.classList.add('container');
-    yangiDiv.style.left = e.clientX + 'px';
-    yangiDiv.style.top = e.clientY + 'px';
-    yangiDiv.style.backgroundColor = color;
-})
+});
+// })
 
 
