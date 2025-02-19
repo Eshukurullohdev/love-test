4523bf62d1d93c691e75df4cd90d7f026a6ecc2a
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-02-19 07:54:17 -0800

    update diff codes

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-const btn = document.querySelector('.btn');
-const container = document.querySelector('.container');
-document.addEventListener('mousemove', (e) => {
-   let yangiDiv = document.createElement('div');
-   document.body.appendChild(yangiDiv);
-   yangiDiv.classList.add('container');
-   yangiDiv.style.left = e.clientX + 'px';
-   yangiDiv.style.top = e.clientY + 'px';
+// const btn = document.querySelector('.btn');
+// const container = document.querySelector('.container');
+// document.addEventListener('mousemove', (e) => {
+//    let yangiDiv = document.createElement('div');
+//    document.body.appendChild(yangiDiv);
+//    yangiDiv.classList.add('container');
+//    yangiDiv.style.left = e.clientX + 'px';
+//    yangiDiv.style.top = e.clientY + 'px';
+// });
+
+let btn = document.querySelector('.btn');
+
+let hexColur = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+btn.addEventListener('click', () => {
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        let random = Math.floor(Math.random() * hexColur.length);
+        color += hexColur[random];
+        console.log(color);
+    }
+    document.body.style.backgroundColor = color;
 });
 
 
