abbba2526d03c171ec0684cda7473cf16271612d
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-03-03 23:36:52 -0800

    color

--- main.js
+++ main.js
@@ -103,3 +103,55 @@
 
 
 
+// console.log(Math.floor(3.9));
+// console.log(Math.floor(-5.8));
+// console.log(Math.floor(-9.9));
+// console.log(Math.floor(9));
+
+
+// function roll() {
+//     return Math.floor(Math.random() * 6);
+// }
+// console.log(roll());
+
+// function rolls(r) {
+//     let index = Math.floor(Math.random() * r.length);
+//     return r[index];
+// }
+
+// let fruits = [
+//     'kiwi', 'olma', 'nok', 'behi'
+// ]
+// console.log(rolls(fruits));
+
+
+// function rall() {
+//     return Math.random() < 0.5;
+// }
+
+// console.log(rall());
+
+
+// function getRandom() {
+//     let r = Math.floor(Math.random() * 256)
+//     let g = Math.floor(Math.random() * 256)
+//     let b = Math.floor(Math.random() * 256)
+//     return `rgb(${r},${g},${b})`
+// }
+// document.body.style.backgroundColor = getRandom();
+
+let hexColur = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+function getRandom() {
+    let color = '#';
+    for(let x = 0; x < 6; x++) {
+        let random = Math.floor(Math.random() * hexColur.length);
+        color += hexColur[random];
+    }
+    return color;
+}
+let btn =  document.querySelector('.btn')
+
+btn.addEventListener('click', () => {
+    document.body.style.backgroundColor = getRandom();
+})
