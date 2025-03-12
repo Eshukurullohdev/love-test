8c0df581c95c28c0ca53b13349dd687cd8ed6259
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-03-12 22:59:35 +0500

    localhost

---
+++ js.js
@@ -0,0 +1,29 @@
+// localStorage.setItem('name', 'John');
+
+// let ism = localStorage.getItem('name');
+// localStorage.removeItem('name');
+
+
+// let user = {ism: 'omatbek', yosh: 20};
+// localStorage.setItem('user', JSON.stringify(user))
+
+ // Sayt yuklanganda saqlangan ismini chiqarish
+ window.onload = function() {
+    let savedName = localStorage.getItem("name");
+    if (savedName) {
+        document.getElementById("savedName").innerText = savedName;
+    }
+};
+
+// Ismni localStorage ga saqlash
+function saveName() {
+    let name = document.getElementById("nameInput").value;
+    localStorage.setItem("name", name);
+    document.getElementById("savedName").innerText = name;
+}
+
+// Ismni o'chirish
+function clearName() {
+    localStorage.removeItem("name");
+    document.getElementById("savedName").innerText = "";
+}
