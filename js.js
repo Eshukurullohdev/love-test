5d6ca2495de8c5d258700dc46402791a560642d7
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-03-13 13:44:22 +0500

    update

--- js.js
+++ js.js
@@ -8,22 +8,22 @@
 // localStorage.setItem('user', JSON.stringify(user))
 
  // Sayt yuklanganda saqlangan ismini chiqarish
- window.onload = function() {
-    let savedName = localStorage.getItem("name");
-    if (savedName) {
-        document.getElementById("savedName").innerText = savedName;
-    }
-};
+//  window.onload = function() {
+//     let savedName = localStorage.getItem("name");
+//     if (savedName) {
+//         document.getElementById("savedName").innerText = savedName;
+//     }
+// };
 
-// Ismni localStorage ga saqlash
-function saveName() {
-    let name = document.getElementById("nameInput").value;
-    localStorage.setItem("name", name);
-    document.getElementById("savedName").innerText = name;
-}
+// // Ismni localStorage ga saqlash
+// function saveName() {
+//     let name = document.getElementById("nameInput").value;
+//     localStorage.setItem("name", name);
+//     document.getElementById("savedName").innerText = name;
+// }
 
-// Ismni o'chirish
-function clearName() {
-    localStorage.removeItem("name");
-    document.getElementById("savedName").innerText = "";
-}
+// // Ismni o'chirish
+// function clearName() {
+//     localStorage.removeItem("name");
+//     document.getElementById("savedName").innerText = "";
+// }
